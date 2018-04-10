230e3e14315f334118544e52357e439853a705ec
SheepYang <332594623@qq.com>
2018-04-11 00:39:28 +0800

    修改左侧菜单名称

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -12,6 +12,12 @@ class BaseSetting(object):
     use_bootswatch = True
 
 
+class GolbalSettings(object):
+    site_title = '绿洲教育后台管理系统'
+    site_footer = '绿洲科技'
+    menu_style= 'accordion'
+
+
 class EmailVerifyRecordAdmin(object):
     list_display = ['code', 'email', 'send_type', 'send_time']
     search_fields = ['code', 'email', 'send_type']
@@ -27,3 +33,4 @@ class BannerAdmin(object):
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
 xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GolbalSettings)
